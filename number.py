70c7b8b43c5bed4c7cf0e4793b27554c9fe5dcb1
Javier <javier@javier.sg>
2020-11-28 00:38:28 +0800

    Guess the number

--- number.py
+++ number.py
@@ -7,18 +7,19 @@ def main():
   number_max = 99
   number = random.randint(number_min,number_max)
 
-  number_players = int(input('How many players? (2-4)'))
+  #number_players = int(input('How many players? (2-4)'))
+  #number_playerlist = ["A", "B", "C", "D"]
 
-  while number_players < 2 or number_players >4:
-    number_players = int(input('You can only have 2-4 players. How many players?'))
+  #while number_players < 2 or number_players >4:
+    #number_players = int(input('You can only have 2-4 players. How many players?'))
 
-  print(f'Great! We have {number_players} players today!')
+  #print(f'Great! We have {number_players} players today!')
 
   if number_debug == 1:
     print(f'Secret number {number} has been initialized!')
   else:
     print('Secret number has been initialized!')
-  number_guess = int(input(f'Guess the number ({number_min} to {number_max})'))
+  number_guess = int(input(f'Guess the number ({number_min} to {number_max}):'))
 
   while number != number_guess:
     if number_guess < number_min:
@@ -34,23 +35,7 @@ def main():
     number_guess = int(input(f'Guess the number ({number_min} to {number_max})'))
 
     if number_guess == number:
-      print(f'You got it right! The number was {number}!')
-
-
-
-  # dice_rolls = int(input('How many dice would you like to roll? '))
-  # dice_size = int(input('How many sides are the dice? '))
-  # dice_sum = 0
-  # for i in range(0,dice_rolls):
-  #   roll = random.randint(1,dice_size)
-  #   dice_sum += roll
-  #   if roll == 1:
-  #     print(f'You rolled a {roll}! Critical Fail')
-  #   elif roll == 6:
-  #     print(f'You rolled a {roll}! Critical Success!')
-  #   else:
-  #     print(f'You rolled a {roll}')
-  # print(f'You have rolled a total of {dice_sum}')
+      print(f'You lose! The number was {number}!')
 
 if __name__== "__main__":
   main()
