7d6f27c0a631a147a0e4cb4f85317b888f3d8b49
Javier <javier@javier.sg>
2020-11-27 21:33:50 +0800

    Added number of players, next add turns

--- number.py
+++ number.py
@@ -1,19 +1,30 @@
 import random
 
 def main():
+  number_debug = 0 #1 to show, 0 to hide
+
   number_min = 1
   number_max = 99
   number = random.randint(number_min,number_max)
-  print(f'Secret number {number} has been initialized!')
+
+  number_players = int(input('How many players? (2-4)'))
+
+  while number_players < 2 or number_players >4:
+    number_players = int(input('You can only have 2-4 players. How many players?'))
+
+  print(f'Great! We have {number_players} players today!')
+
+  if number_debug == 1:
+    print(f'Secret number {number} has been initialized!')
+  else:
+    print('Secret number has been initialized!')
   number_guess = int(input(f'Guess the number ({number_min} to {number_max})'))
 
   while number != number_guess:
     if number_guess < number_min:
       print(f'You number cannot be less than {number_min}. Try again!')
-      #continue
     elif number_guess > number_max:
       print(f'You number cannot be more than {number_max}. Try again!')
-      #continue
     else:
       if number_guess < number:
         number_min = number_guess
